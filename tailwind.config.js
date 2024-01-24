5eeb55b9cb4e8278d126c2ec95af19a4d9fbef4e
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2024-01-24 13:13:36 +0600

    shadow design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': '0 0px 0px -15px rgba(0, 0, 0, 0.3)',
+      }
+    }
   },
   plugins: [],
 }
