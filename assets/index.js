514b78ce7c326bb4a6b0949eba62d17c209e2daa
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2024-01-24 16:07:19 +0600

    search destination

--- assets/index.js
+++ assets/index.js
@@ -6,6 +6,20 @@ const handleClickSearchDestination = () => {
     $(".search_destination").click(function () {
         $(".search_destination_main").addClass('hidden');
         $(".search_destination_form").removeClass('hidden');
+        $(".search_destination").focus();
+    });
+
+    $(".search_destination").keyup(function () {
+        const value = $(this).val().toLowerCase();
+        $(".destination_option").each(function () {
+            const text = $(this).text().toLowerCase();
+            if (text.search(value) > -1) {
+                $(this).show();
+            }
+            else {
+                $(this).hide();
+            }
+        });
     });
 
     $(".return_main_destination_form").click(function () {
@@ -17,6 +31,6 @@ const handleClickSearchDestination = () => {
         $(".search_destination_form").addClass('hidden');
         $(".search_destination_main").removeClass('hidden');
         const value = $(this).find('option').val();
-        $(".search_destination").val(value).addClass('text-lg font-semibold');
+        $(".search_destination").val(value).addClass('text-xl font-semibold');
     });
 };
