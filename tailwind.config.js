5f856d8b1c358470fb4960caba07d630dc171ed6
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2024-01-24 13:15:32 +0600

    working on option

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      boxShadow: {
-        'custom': '0 0px 0px -15px rgba(0, 0, 0, 0.3)',
-      }
     }
   },
   plugins: [],
