1cf4da01bbe296abf26874728f256ca605883e0b
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2024-02-12 17:36:14 +0600

    working btn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      '2xs': '320px',
+      'xs': '375px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily:{
         'rubik': ['Rubik', 'sans-serif']
